2e62386f6302e6410fbc10c4bb9ef47134cd79c9
catsduck <7939228+catsduck@users.noreply.github.com>
2022-02-02 09:56:02 -0500

    renamed user

--- scripts/ubuntu-proxmox.sh
+++ scripts/ubuntu-proxmox.sh
@@ -10,18 +10,18 @@ apt-get upgrade -y
 apt-get autoremove
 apt-get clean
 
-# create and setup terraform user
-groupadd terraform
-useradd -m -g terraform -s /bin/bash terraform
-mkdir /home/terraform/.ssh
-cat << EOF > /home/terraform/.ssh/authorized_keys
+# create and setup ubuntu user
+groupadd ubuntu
+useradd -m -g ubuntu -s /bin/bash ubuntu
+mkdir /home/ubuntu/.ssh
+cat << EOF > /home/ubuntu/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+nwxFCRrhxk0eCZVyn7fdOm9/Ejjk8TDYTeUatG0nhX0zXEPl/oiUvZsRQdagJb43re59cYeNugegLpZZbbJIpO8P3EFw02MhxwOTHA+xrQHxW4alRUu/iVeaQtxU0lz+jlKKltD5dlq8SHBk3pR3vP0TL/96uPw0FXNv69rztnJNiM9f7bUhhLBKCPhzPVJL/rEIF09+tEzVT6BTRCnrY4Xtj8c3ch87ToCY5bh71TKxBdHDU3xFmG/Hqz2qFHVNZ2EIXkuw+EaNgWQG3a2dAW7C6lcxO9Wm1OKPAQ67+qaYWgn7S4kqYd1DGhzPIzkwoUh2H4V1GrVSi99cj8d0BryBmRTHS3OhUf/uYwASiiA6vaqiqGO3qK8WGtYvzk3ZnkGe1unpUnKBg5MSLomrBaDe94P589BVVi3vjGY7AS4cOF7RafyO+KetrkGMUwUTVqTlfjSYWEk7clfPp3XaeGgTy/Xd14GiEX86IMGkZ1AnVjZYm9eegY2PCd6agA8=
 EOF
-chown -R terraform:terraform /home/terraform/.ssh
-chmod 700 /home/terraform/.ssh
-chmod 600 /home/terraform/.ssh/authorized_keys
-echo "terraform ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/terraform
-chmod 440 /etc/sudoers.d/terraform
+chown -R ubuntu:ubuntu /home/ubuntu/.ssh
+chmod 700 /home/ubuntu/.ssh
+chmod 600 /home/ubuntu/.ssh/authorized_keys
+echo "ubuntu ALL=(ALL:ALL) NOPASSWD:ALL" > /etc/sudoers.d/ubuntu
+chmod 440 /etc/sudoers.d/ubuntu
 
 # remote machine-id so it gets regenerated and avoids duplicates
 >/etc/machine-id
